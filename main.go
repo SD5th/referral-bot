12fd65e316705a25547e9a8fb57cbb40c336f38a
SD5th <vlaka@ro.ru>
2025-11-07 01:20:46 +0300

    From echobot to raw-base

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 // Config - структура для хранения конфигурации
 type Config struct {
 	Token string `json:"token"`
+	Debug bool   `json:"debug"`
 }
 
 // loadConfig загружает конфигурацию из config.json
@@ -36,22 +37,84 @@ func loadConfig() (*Config, error) {
 	return &config, nil
 }
 
+func Must[T any](v T, err error) T {
+	if err != nil {
+		log.Fatalf("Ошибка: %v", err)
+	}
+	return v
+}
+
+func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	// Логируем полученное сообщение
+	log.Printf("[%s] %s", message.From.UserName, message.Text)
+
+	// Создаем ответное сообщение (эхо)
+	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
+
+	// Отправляем сообщение
+	_, err := bot.Send(msg)
+	if err != nil {
+		log.Printf("Ошибка отправки сообщения: %v", err)
+	}
+}
+
+func handleEditedMessage(bot *tgbotapi.BotAPI, editedMessage *tgbotapi.Message)                {}
+func handleChannelPost(bot *tgbotapi.BotAPI, channelPost *tgbotapi.Message)                    {}
+func handleEditedChannelPost(bot *tgbotapi.BotAPI, editedChannelPost *tgbotapi.Message)        {}
+func handleInlineQuery(bot *tgbotapi.BotAPI, inlineQuery *tgbotapi.InlineQuery)                {}
+func handleChosenInlineResult(bot *tgbotapi.BotAPI, inlineResult *tgbotapi.ChosenInlineResult) {}
+func handleCallbackQuery(bot *tgbotapi.BotAPI, callbackQuery *tgbotapi.CallbackQuery)          {}
+func handleShippingQuery(bot *tgbotapi.BotAPI, shippingQuery *tgbotapi.ShippingQuery)          {}
+func handlePreCheckoutQuery(bot *tgbotapi.BotAPI, preCheckoutQuery *tgbotapi.PreCheckoutQuery) {}
+func handlePoll(bot *tgbotapi.BotAPI, poll *tgbotapi.Poll)                                     {}
+func handlePollAnswer(bot *tgbotapi.BotAPI, pollAnswer *tgbotapi.PollAnswer)                   {}
+func handleMyChatMember(bot *tgbotapi.BotAPI, myChatMember *tgbotapi.ChatMemberUpdated)        {}
+func handleChatMember(bot *tgbotapi.BotAPI, chatMember *tgbotapi.ChatMemberUpdated)            {}
+func handleChatJoinRequest(bot *tgbotapi.BotAPI, chatJoinRequest *tgbotapi.ChatJoinRequest)    {}
+
+func handleUpdate(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	switch {
+	case update.Message != nil:
+		handleMessage(bot, update.Message)
+	case update.EditedMessage != nil:
+		handleEditedMessage(bot, update.EditedMessage)
+	case update.ChannelPost != nil:
+		handleChannelPost(bot, update.ChannelPost)
+	case update.EditedChannelPost != nil:
+		handleEditedChannelPost(bot, update.EditedChannelPost)
+	case update.InlineQuery != nil:
+		handleInlineQuery(bot, update.InlineQuery)
+	case update.ChosenInlineResult != nil:
+		handleChosenInlineResult(bot, update.ChosenInlineResult)
+	case update.CallbackQuery != nil:
+		handleCallbackQuery(bot, update.CallbackQuery)
+	case update.ShippingQuery != nil:
+		handleShippingQuery(bot, update.ShippingQuery)
+	case update.PreCheckoutQuery != nil:
+		handlePreCheckoutQuery(bot, update.PreCheckoutQuery)
+	case update.Poll != nil:
+		handlePoll(bot, update.Poll)
+	case update.PollAnswer != nil:
+		handlePollAnswer(bot, update.PollAnswer)
+	case update.MyChatMember != nil:
+		handleMyChatMember(bot, update.MyChatMember)
+	case update.ChatMember != nil:
+		handleChatMember(bot, update.ChatMember)
+	case update.ChatJoinRequest != nil:
+		handleChatJoinRequest(bot, update.ChatJoinRequest)
+	}
+
+}
+
 func main() {
-	log.Println("Запуск echo-бота...")
+	log.Println("Запуск бота...")
 
 	// Загружаем конфигурацию
-	config, err := loadConfig()
-	if err != nil {
-		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
-	}
+	config := Must(loadConfig())
 
 	// Создаем бота
-	bot, err := tgbotapi.NewBotAPI(config.Token)
-	if err != nil {
-		log.Fatalf("Ошибка создания бота: %v", err)
-	}
-
-	bot.Debug = true // Включаем отладку
+	bot := Must(tgbotapi.NewBotAPI(config.Token))
+	bot.Debug = config.Debug
 	log.Printf("Авторизован как %s", bot.Self.UserName)
 
 	// Настраиваем канал для получения обновлений
@@ -64,21 +127,6 @@ func main() {
 
 	// Обрабатываем входящие сообщения
 	for update := range updates {
-		// Игнорируем любые сообщения, кроме текстовых
-		if update.Message == nil {
-			continue
-		}
-
-		// Логируем полученное сообщение
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		// Создаем ответное сообщение (эхо)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
-		// Отправляем сообщение
-		_, err := bot.Send(msg)
-		if err != nil {
-			log.Printf("Ошибка отправки сообщения: %v", err)
-		}
+		handleUpdate(bot, update)
 	}
 }
